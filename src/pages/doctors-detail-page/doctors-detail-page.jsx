6c6424daba2a1cb89078088ec725dc7a34b9fed1
Rishat23 <rishatyaparov23@gmail.com>
2023-12-26 17:10:30 +0500

    Исправил почти все

--- src/pages/doctors-detail-page/doctors-detail-page.jsx
+++ src/pages/doctors-detail-page/doctors-detail-page.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
 
+import DoctorsDetailContainer from '@components/doctors-detail-container'
+import Education from '@components/doctors-detail-container/education'
 import EmployeesSlider from '@components/employees-slider'
 
-import DoctorsDetailContainer from '../../components/Doctors-detail-container/doctors-detail-container.jsx'
-import OurWorks from '../../components/OurWorks/OurWorks.jsx'
+import OurWorks from '../../components/our-works/our-works.jsx'
+import MainLayout from '../../layout/main/index.js'
 
 const DoctorsDetailPage = () => {
 	return (
-		<>
+		<MainLayout>
 			<DoctorsDetailContainer />
+			<Education />
 			<OurWorks />
 			<EmployeesSlider sliderHeaderText="Врачи по направлению" />
-		</>
+		</MainLayout>
 	)
 }
 
