88e79dd4e5e93de0b10c561bff4d53f64059500f
Rishat23 <rishatyaparov23@gmail.com>
2024-01-12 14:25:11 +0500

    почти слелал стронички врочей

--- src/components/doctors-detail-container/hero-screen/hero-screen.jsx
+++ src/components/doctors-detail-container/hero-screen/hero-screen.jsx
@@ -1,8 +1,10 @@
 import portrait from '@media/heroScreen/aliya1920.jpg'
+import { useGetDoctorQuery } from '@store/modules/doctors-api.js'
 import { setSignUpIsOpen } from '@store/modules/modals.js'
 import cn from 'classnames'
-import React, { useMemo } from 'react'
+import React from 'react'
 import { useDispatch } from 'react-redux'
+import { useParams } from 'react-router-dom'
 
 import Button from '@ui/button/index.js'
 
@@ -14,41 +16,32 @@ const HeroScreen = () => {
 	const openModalHandler = () => {
 		dispatch(setSignUpIsOpen(true))
 	}
-
-	const doctorsList = useMemo(
-		() => [
-			{
-				id: 0,
-				// img: '/media/heroScreen/aliya1920.jpg',
-				// classImg: cl.detailDoctorImage,
-				name: 'Алия Беглова',
-				qualification: 'Врач-ортодонт',
-				price: 'от 2500',
-				information: 'Взрослый и детский врач'
-			}
-		],
-		[]
-	)
+	const { id: doctorId } = useParams()
+	const { data: doctorsList = [] } = useGetDoctorQuery(doctorId)
 
 	return (
 		<div className={cl.detailHeroScreen}>
 			<div className="container">
-				{doctorsList.map(({ id, name, qualification, price, information }) => (
+				{doctorsList.map(({ id, name, image, speciality, price, direction }) => (
 					<div key={id} className={cn([cl.detailInformation, 'd-flex', 'flex-column-reverse', 'flex-md-row'])}>
 						<div className={cn([cl.detailInformationBlock, 'd-flex', 'flex-column'])}>
 							<div className={cl.detailInformationBlockName}>{name}</div>
-							<div className={cl.detailInformationBlockQualification}>{qualification}</div>
-							<div className={cl.detailInformationBlockPrice}>{price}</div>
-							<div className={cn(cl.detailInformationBlockInformation, 'd-flex')}>{information}</div>
+							<div className={cl.detailInformationBlockQualification}>
+								{speciality.map(({ name: specialityName }) => specialityName)}
+							</div>
+							<div className={cl.detailInformationBlockPrice}>от {price} ₽</div>
+							<div className={cn(cl.detailInformationBlockInformation, 'd-flex')}>
+								{direction.map(({ name: directionName }) => directionName)}
+							</div>
 							<div className={cl.bckgIcon} />
 							<div className={cn([cl.appointment, 'd-flex', 'flex-column'])}>
 								<Button onClick={openModalHandler} className={cl.btn}>
 									Записаться
 								</Button>
-								<div className={cl.appointmentText}>Ближайшая запись врача: с 1 декабря в 09:00</div>
+								<div className={cl.appointmentText}>Ближайшая запись врача: с 1 декабря в 09:00</div>
 							</div>
 						</div>
-						<img src={portrait} className={cl.detailInformationPortrait} alt="" />
+						<img src={image} className={cl.detailInformationPortrait} alt="" />
 						{/* <img src={img} className={cl.detailInformationPortrait} alt="doctorImage" /> */}
 						{/* <div className={cl.detailInformationPortrait} /> */}
 					</div>
