b68531446da8943993cc940c1eab93c11ee3357c
sl1dee <118618836+sl1dee@users.noreply.github.com>
2024-02-16 17:38:00 +0500

    добавил компонент cookies

--- src/components/cookie/cookie.jsx
+++ src/components/cookie/cookie.jsx
@@ -1,21 +1,30 @@
-import cn from 'classnames'
 import React, { useState } from 'react'
-
-import cl from './cookie.module.scss'
+import { CookieConsent } from 'react-cookie-consent'
 
 const Cookie = () => {
 	const [accept, setAccept] = useState(false)
 
 	return (
-		<div className={cn([cl.cookie, `${accept === true ? ' d-none' : ''}`])}>
-			{/* <div className={`${cl.cookie}${accept === true ? ' d-none' : ''}`}> */}
-			<div className="container d-flex justify-content-between align-items-center">
-				<span className={cl.cookieText}>Мы используем cookies</span>
-				<button className={cl.cookieButton} onClick={() => setAccept(!accept)}>
-					Хорошо
-				</button>
-			</div>
-		</div>
+		// <div className={cn([cl.cookie, `${accept === true ? ' d-none' : ''}`])}>
+		// 	{/* <div className={`${cl.cookie}${accept === true ? ' d-none' : ''}`}> */}
+		// 	<div className="container d-flex justify-content-between align-items-center">
+		// 		<span className={cl.cookieText}>Мы используем cookies</span>
+		// 		<button className={cl.cookieButton} onClick={() => setAccept(!accept)}>
+		// 			Хорошо
+		// 		</button>
+		// 	</div>
+		// </div>
+
+		<CookieConsent
+			location="bottom"
+			buttonText="Хорошо"
+			cookieName="myAwesomeCookieName2"
+			style={{ background: '#2B373B' }}
+			buttonStyle={{ color: '#4e503b', fontSize: '13px' }}
+			expires={150}
+		>
+			Мы используем cookies {/* <span style={{ fontSize: '10px' }}>This bit of text is smaller :O</span> */}
+		</CookieConsent>
 	)
 }
 
